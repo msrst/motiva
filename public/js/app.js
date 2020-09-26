a7778e59d15f2fda8ec6a5796f560c4f809f56bd
emely <emely3h@gmail.com>
2020-09-26 20:57:23 +0200

    bla

--- public/js/app.js
+++ public/js/app.js
@@ -42155,8 +42155,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\resources\css\app.css */"./resources/css/app.css");
+__webpack_require__(/*! C:\projects\motiva\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\projects\motiva\resources\css\app.css */"./resources/css/app.css");
 
 
 /***/ })
