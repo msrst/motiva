ce018611a408a8d860608c04314a31c7355a672a
alexn9630 <58658679+alexn9630@users.noreply.github.com>
2020-09-27 03:22:04 +0200

    random

--- resources/js/Pages/Profile/Show.vue
+++ resources/js/Pages/Profile/Show.vue
@@ -7,28 +7,17 @@
         </template>
 
         <div>
-            <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+            <avatar>
+            </avatar>
+            <jet-button type="button" @click.native="showExtraInformation=!showExtraInformation">
+                Account Settings
+            </jet-button>   
+            <div v-if="showExtraInformation" class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
                 <update-profile-information-form
                             :name="$page.user.name"
                             :email="$page.user.email" />
 
-                <jet-section-border />
-
                 <update-password-form class="mt-10 sm:mt-0" />
-
-                <div v-if="$page.jetstream.canManageTwoFactorAuthentication">
-                    <jet-section-border />
-
-                    <two-factor-authentication-form class="mt-10 sm:mt-0" />
-                </div>
-
-                <jet-section-border />
-
-                <logout-other-browser-sessions-form :sessions="sessions" class="mt-10 sm:mt-0" />
-
-                <jet-section-border />
-
-                <delete-user-form class="mt-10 sm:mt-0" />
             </div>
         </div>
     </app-layout>
@@ -36,24 +25,25 @@
 
 <script>
     import AppLayout from './../../Layouts/AppLayout'
-    import DeleteUserForm from './DeleteUserForm'
-    import JetSectionBorder from './../../Jetstream/SectionBorder'
-    import LogoutOtherBrowserSessionsForm from './LogoutOtherBrowserSessionsForm'
-    import TwoFactorAuthenticationForm from './TwoFactorAuthenticationForm'
     import UpdatePasswordForm from './UpdatePasswordForm'
     import UpdateProfileInformationForm from './UpdateProfileInformationForm'
+    import JetButton from  './../../Jetstream/Button'
+    import Avatar from './../../Components/Avatar'
 
     export default {
         props: ['sessions'],
 
         components: {
             AppLayout,
-            DeleteUserForm,
-            JetSectionBorder,
-            LogoutOtherBrowserSessionsForm,
-            TwoFactorAuthenticationForm,
             UpdatePasswordForm,
             UpdateProfileInformationForm,
+            JetButton,
+            Avatar
         },
+        data(){
+            return {
+                showExtraInformation: false
+            }
+        }
     }
 </script>
