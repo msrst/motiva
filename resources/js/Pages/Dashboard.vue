96bef631a6e90f90e181a3a8776a563cdc828f21
ChaI-LacK <58135214+ChaI-LacK@users.noreply.github.com>
2020-09-27 04:44:42 +0200

    dashboard stuff

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,80 +1,142 @@
 <template>
-  <app-layout>
-      <template slot="header">
-          Dashboard
-      </template>
-      <template>
-          <!-- component -->
-<li class="flex flex-row">
-<div class="max-w-sm shadow-lg rounded-lg overflow-hidden my-4 bg-gray-300 ml-32">
-        <img class="w-32 h-320 mx-auto object-cover object-center" src="https://media.discordapp.net/attachments/759407593822093332/759520636740960276/untitled_4.png" alt="avatar">
-        <div class="px-6 py-3 bg-gray-700">
-            <h1 class="text-center text-white font-semibold text-lg">{{$page.user.name}}</h1>
-        </div>
+    <app-layout>
+        <template slot="header">
+            Dashboard
+        </template>
+        <template>
+            <!-- component -->
+            <li class="flex flex-row">
+                <div
+                    class="max-w-sm shadow-lg rounded-lg overflow-hidden my-4 bg-gray-300 ml-32"
+                >
+                    <img
+                        class="w-32 h-320 mx-auto object-cover object-center"
+                        src="https://media.discordapp.net/attachments/759407593822093332/759520636740960276/untitled_4.png"
+                        alt="avatar"
+                    />
+                    <div class="px-6 py-3 bg-gray-700">
+                        <h1
+                            class="text-center text-white font-semibold text-lg"
+                        >
+                            {{ $page.user.name }}
+                        </h1>
+                    </div>
 
-        <!-- component -->
-   <ul class="flex flex-col p-4">
-        <li class="border-gray-400 flex flex-row mb-2">
+                    <!-- component -->
+                    <ul class="flex flex-col p-4">
+                        <li class="border-gray-400 flex flex-row mb-2">
+                            <div
+                                class="select-none cursor-pointer bg-gray-200 rounded-md flex flex-direction-right items-center p-4  transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-lg mr-3"
+                            >
+                                <div class="flex-1 pl-1 mr-32">
+                                    <div class="font-medium">Daily Points</div>
+                                </div>
+                                <div class="text-gray-600 text-s">
+                                    {{ $page.user.dailypoints }}/10
+                                </div>
+                            </div>
+                            <div class="mx-auto pt-3">
+                                <font-awesome-icon
+                                    icon="sad-tear"
+                                    v-if="$page.user.dailypoints < 10"
+                                    size="3x"
+                                    class="p-2 text-yellow bg-gray-400 rounded-md"
+                                ></font-awesome-icon>
+                                <font-awesome-icon
+                                    icon="check-circle"
+                                    v-else
+                                    size="3x"
+                                    class="text-green"
+                                ></font-awesome-icon>
+                            </div>
+                        </li>
+                        <li class="border-gray-400 flex flex-row mb-2">
+                            <div
+                                class="select-none cursor-pointer bg-gray-200 rounded-md flex flex-1 items-center p-4  transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-lg"
+                            >
+                                <div class="flex-1 pl-1 mr-16">
+                                    <div class="font-medium">Weekly Points</div>
+                                </div>
+                                <div class="text-gray-600 text-s">
+                                    {{ $page.user.weeklypoints }}
+                                </div>
+                            </div>
+                        </li>
+                        <li class="border-gray-400 flex flex-row mb-2">
+                            <div
+                                class="select-none cursor-pointer bg-gray-200 rounded-md flex flex-1 items-center p-4  transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-lg"
+                            >
+                                <div class="flex-1 pl-1 mr-16">
+                                    <div class="font-medium">Total Points</div>
+                                </div>
+                                <div class="text-gray-600 text-s">
+                                    {{ $page.user.totalpoints }}
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <ul>
+                    <div
+                        class="w-full shadow-lg rounded-lg overflow-hidden my-4 bg-gray-300 ml-32 mt-16"
+                    >
+                        <h2
+                            class="ml-4 text-gray-600 text-3xl font-weight-bold"
+                        >
+                            Tasks
+                        </h2>
+                        <div class="px-64 py-8 bg-gray-300">
+                            <div class="px-full shadow bg-grey-light">
+                                <div
+                                    class="bg-green text-s leading-none py-2 text-center text-white"
+                                    :class="progressClasses"
+                                >
+                                    Progress
+                                </div>
+                            </div>
+                        </div>
 
-          <div class="select-none cursor-pointer bg-gray-200 rounded-md flex flex-direction-right items-center p-4  transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-lg">
-            <div class="flex-1 pl-1 mr-32">
-              <div class="font-medium">Daily Points</div>
-            </div>
-            <div class="text-gray-600 text-s">{{$page.user.dailypoints}}/10</div>
-          </div>
-            <div class="mx-auto pt-3">
-                <font-awesome-icon icon="sad-tear" v-if="$page.user.dailypoints<10" size="3x" class="p-2 text-yellow bg-gray-400 rounded-md" ></font-awesome-icon>
-                <font-awesome-icon icon="check-circle" v-else size="3x" class="text-green"></font-awesome-icon>
-            </div>
-        </li>
-             <li class="border-gray-400 flex flex-row mb-2">
-          <div class="select-none cursor-pointer bg-gray-200 rounded-md flex flex-1 items-center p-4  transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-lg">
-            <div class="flex-1 pl-1 mr-16">
-              <div class="font-medium">Weekly Points</div>
-            </div>
-            <div class="text-gray-600 text-s">{{$page.user.weeklypoints}}</div>
-          </div>
-        </li>
-             <li class="border-gray-400 flex flex-row mb-2">
-          <div class="select-none cursor-pointer bg-gray-200 rounded-md flex flex-1 items-center p-4  transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-lg">
-            <div class="flex-1 pl-1 mr-16">
-              <div class="font-medium">Total Points</div>
-            </div>
-            <div class="text-gray-600 text-s">{{$page.user.totalpoints}}</div>
-          </div>
-        </li>
-    </ul>
-    </div>
-    <ul>
-    <div class="h-3200 w-320  shadow-lg rounded-lg overflow-hidden my-4 bg-gray-300 ml-32">
-        <div class="px-6 py-3 bg-gray-300">
-        </div>
+                        <!-- component -->
+                    </div>
+                    <div
+                        class="w-full shadow-lg rounded-lg overflow-hidden my-4 bg-gray-300 ml-32 mt-16"
+                    >
+                        <div class="px-64 py-48 bg-gray-300"></div>
 
-        <!-- component -->
-
-    </div>
-    <div class="h-3200 w-320 shadow-lg rounded-lg overflow-hidden my-4 bg-gray-300 ml-32">
-        <div class="px-6 py-3 bg-gray-300">
-        </div>
-
-        <!-- component -->
-
-    </div>
-    </ul>
-    </li>
-      </template>
-  </app-layout>
+                        <!-- component -->
+                    </div>
+                </ul>
+            </li>
+        </template>
+    </app-layout>
 </template>
-
 
 <script>
 import AppLayout from "./../Layouts/AppLayout";
 import Welcome from "./../Jetstream/Welcome";
 
 export default {
-  components: {
-    AppLayout,
-    Welcome,
-  },
+    components: {
+        AppLayout,
+        Welcome
+    },
+
+    computed: {
+        progressClasses() {
+            const progress = $page.user.dailypoints / 10;
+            console.log(progress);
+            if (progress == 0) return "w-0";
+            if (progress == 0.1) return "w-1/10";
+            if (progress == 0.2) return "w-1/5";
+            if (progress == 0.3) return "w-3/10";
+            if (progress == 0.4) return "w-2/5";
+            if (progress == 0.5) return "w-1/2";
+            if (progress == 0.6) return "w-3/5";
+            if (progress == 0.7) return "w-7/10";
+            if (progress == 0.8) return "w-4/5";
+            if (progress == 0.9) return "w-9/10";
+            return "w-full";
+        }
+    }
 };
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,29 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
     purge: [
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
+        "./vendor/laravel/jetstream/**/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.vue"
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ["Nunito", ...defaultTheme.fontFamily.sans]
             },
             colors: {
-                green: '#1aae9f',
-                yellow: '#F7C325',
-                red: '#de3618',
+                green: "#1aae9f",
+                yellow: "#F7C325",
+                purple: "#730FC3"
             }
-        },
-
+        }
     },
 
     variants: {
-        opacity: ['responsive', 'hover', 'focus', 'disabled'],
+        opacity: ["responsive", "hover", "focus", "disabled"]
     },
 
-    plugins: [require('@tailwindcss/ui')],
-
-
+    plugins: [require("@tailwindcss/ui")]
 };
